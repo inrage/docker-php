47d0756a1874f1f9f7bcf1e17ca4eeae9128aa65
Pascal GAULT <pascal@inrage.fr>
2024-10-20 21:22:16 +0200

    fix(image): apply apache2 conf directly in conf-enabled folder

--- latest/php8.2/apache/docker-entrypoint.sh
+++ latest/php8.2/apache/docker-entrypoint.sh
@@ -3,23 +3,22 @@
 set -eo pipefail
 
 if [[ -n "${DEBUG}" ]]; then
-    set -x
+  set -x
 fi
 
 _gotpl() {
-    if [[ -f "/etc/gotpl/$1" ]]; then
-        gotpl "/etc/gotpl/$1" > "$2"
-    fi
+  if [[ -f "/etc/gotpl/$1" ]]; then
+    gotpl "/etc/gotpl/$1" >"$2"
+  fi
 }
 
 exec_init_scripts() {
-    shopt -s nullglob
-    for f in /docker-entrypoint-init.d/*; do
-        . "$f"
-    done
-    shopt -u nullglob
+  shopt -s nullglob
+  for f in /docker-entrypoint-init.d/*; do
+    . "$f"
+  done
+  shopt -u nullglob
 }
-
 
 process_templates() {
   _gotpl "docker-php-base.ini.tmpl" "${PHP_INI_DIR}/conf.d/zzz-custom-php.ini"
@@ -27,8 +26,7 @@ process_templates() {
   _gotpl "docker-php-ext-opcache.ini.tmpl" "${PHP_INI_DIR}/conf.d/docker-php-ext-opcache.ini"
 
   _gotpl "docker-apache-vhost.conf.tmpl" "/etc/apache2/sites-available/000-default.conf"
-  _gotpl "docker-apache-remoteip.conf.tmpl" "/etc/apache2/conf-available/remoteip.conf"
-
+  _gotpl "docker-apache-remoteip.conf.tmpl" "/etc/apache2/conf-enabled/remoteip.conf"
 
   _gotpl "msmtprc.tmpl" "/etc/msmtprc"
 }
@@ -36,6 +34,5 @@ process_templates() {
 process_templates
 
 exec_init_scripts
-
 
 exec /usr/local/bin/docker-php-entrypoint "${@}"
